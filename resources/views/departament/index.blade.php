36fcfa9aa4b88f620872330f6a589099221d22d0
Fausto <fausto@group1245.com>
2025-10-05 21:35:42 -0300

    feat: add index views for Departament, Product, and Search with basic structure

---
+++ resources/views/departament/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Departamentos - Monitor de Preços')
+@section('description', 'Encontre os melhores preços em diversos departamentos. Compare ofertas e economize nas suas compras.')
+@section('content')
+@endsection
+@push('scripts')
+@endpush
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Produto - Monitor de Preços')
+@section('description', 'Encontre os melhores preços em diversos departamentos. Compare ofertas e economize nas suas compras.')
+@section('content')
+@endsection
+@push('scripts')
+@endpush

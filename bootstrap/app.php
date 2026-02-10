c3618569c3d9c6c85986793c8755f9d46d0ed0d8
Fausto <fausto@group1245.com>
2026-02-10 02:43:02 -0300

    refactor: add trustProxies middleware configuration for improved proxy handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'track.browsing' => \App\Http\Middleware\TrackBrowsingHistory::class,
         ]);
         $middleware->redirectGuestsTo('auth/login');
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

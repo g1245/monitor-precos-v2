2308e7c1906bbc6c285cace270319d3ddd2298d0
Fausto <fausto@group1245.com>
2026-02-18 22:54:20 -0300

    fix: enhance logging for product processing in CentauroProductProcessor

--- app/Services/ProductProcessors/CentauroProductProcessor.php
+++ app/Services/ProductProcessors/CentauroProductProcessor.php
@@ -20,7 +20,7 @@ class CentauroProductProcessor extends BaseProductProcessor
      */
     public function process(Product $product): void
     {
-        Log::info('Processing product for Centauro', [
+        Log::info('[CentauroProductProcessor] Processing product for Centauro', [
             'product_id' => $product->id,
             'sku' => $product->sku,
         ]);
@@ -32,6 +32,11 @@ class CentauroProductProcessor extends BaseProductProcessor
             // If no custom_4, treat as standalone product (parent)
             $product->is_parent = 0;
             $product->save();
+
+            Log::info('[CentauroProductProcessor] Product has no custom_4 attribute, set as standalone parent', [
+                'product_id' => $product->id,
+            ]);
+            
             return;
         }
 
@@ -50,10 +55,27 @@ class CentauroProductProcessor extends BaseProductProcessor
         // Set is_parent accordingly
         if ($product->id == $parentId) {
             $product->is_parent = 0; // This is the parent
+            
+            Log::info('[CentauroProductProcessor] Product is the parent of the group', [
+                'product_id' => $product->id,
+                'custom_4' => $custom4Value,
+            ]);
+
         } else {
             $product->is_parent = $parentId; // This is a child, reference the parent
+
+            Log::info('[CentauroProductProcessor] Product is a child of the group', [
+                'product_id' => $product->id,
+                'parent_id' => $parentId,
+                'custom_4' => $custom4Value,
+            ]);
         }
 
         $product->save();
+
+        Log::info('[CentauroProductProcessor] Finished processing product for Centauro', [
+            'product_id' => $product->id,
+            'is_parent' => $product->is_parent,
+        ]);
     }
 }

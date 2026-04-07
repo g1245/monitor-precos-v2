8d41fbfcdad8e2ffdd766f0af9bf0853acb3657a
Fausto <fausto@group1245.com>
2026-04-07 17:33:33 -0300

    fix(console): update sync schedule for top discounted products to hourly

--- routes/console.php
+++ routes/console.php
@@ -11,8 +11,8 @@ Schedule::command('app:create-today-price')->dailyAt('00:01');
 // Schedule command to sync product data by store every three hours
 Schedule::command('app:sync-product-by-store')->hourly();
 
-// Schedule command to sync top discounted products to Department 1 every six hours
-Schedule::command('app:sync-top-discounted-products-to-department')->everySixHours();
+// Schedule command to sync top discounted products to Department every hour
+Schedule::command('app:sync-top-discounted-products-to-department')->hourly();
 
 /** 
  * Schedule commands for backup and cleanup of old backups
